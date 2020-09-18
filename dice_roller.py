1cc24352dd73df5d950e27b0d9393cbdbdda32e9
abarnaks <abarnadevi.ks@gmail.com>
2020-09-17 20:34:11 -0400

    generated random number for die in range (1,6)

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
+
 def main():
-  print('You rolled a die')
+
+  roll = random.randint(1,6) 
+
+  #use f and put var inside {} in str to print variable within string
+  print(f'You rolled a {roll}')
+
 
 if __name__== "__main__":
   main()
